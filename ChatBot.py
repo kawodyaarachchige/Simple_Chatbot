04ebad7296ad3a035635470db857f5844eae4cd0
Tharushi Kawodya Arachchige <139765472+kawodyaarachchige@users.noreply.github.com>
2024-09-19 19:42:34 +0530

    final touched up

--- ChatBot.py
+++ ChatBot.py
@@ -3,7 +3,7 @@ from tkinter import scrolledtext
 import re
 
 
-class SmartChatbot:
+class ChatBot:
     def __init__(self):
         self.context = ""
         self.responses = {
@@ -14,13 +14,21 @@ class SmartChatbot:
             'what is your name': 'I am a chatbot created by Tharushi.',
             'default': 'Sorry, I don\'t understand that. Type "help" for assistance.',
             'what is your favorite color': 'I don\'t have a favorite color, but I can help you find out!',
-            'what is your favorite movie': 'I don\'t have a favorite movie, but I can help you find out!'
+            'what is your favorite movie': 'I don\'t have a favorite movie, but I can help you find out!',
+            'love': 'Love is a beautiful feeling! It’s wonderful to talk about it.',
+            'tell me about love': 'Love can be expressed in many ways. It’s all about connection and care.',
+            # 'do you believe in love': 'As a bot, I don’t have feelings, but I understand that love is important to many people.',
+            # 'what do you think about my boyfriend': 'I don’t know your boyfriend, but I’m sure he’s great if you care about him! what is his name?',
+            # 'tell me about my boyfriend': 'I don’t have information about your boyfriend, but it’s great that you care about him!'
         }
+
 
     def get_response(self, user_input):
 
         if 'weather' in user_input:
             return "I can't check the weather, but I can help with other queries!"
+        # if 'kusal' in user_input:
+        #     return 'ah i know him,he is innocent clever, and handsome boy!'
         if 'time' in user_input:
             from datetime import datetime
             return f"The current time is {datetime.now().strftime('%H:%M:%S')}."
@@ -43,7 +51,7 @@ class ChatApp:
         self.chatbot = chatbot
 
         # Set up the chat window
-        self.chat_window = scrolledtext.ScrolledText(root, width=80, height=20, state=tk.DISABLED)
+        self.chat_window = scrolledtext.ScrolledText(root, width=100, height=20, state=tk.DISABLED)
         self.chat_window.pack()
 
         self.entry_box = tk.Entry(root, width=50)
@@ -77,9 +85,9 @@ class ChatApp:
 
 
 root = tk.Tk()
-root.title("Smart Chatbot")
+root.title("Chatbot")
 
-chatbot = SmartChatbot()
+chatbot = ChatBot()
 
 app = ChatApp(root, chatbot)
 
